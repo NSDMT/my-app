a5c10726e5c98c839b10142d1ba80849b0e89b25
NSDMT <114123942+NSDMT@users.noreply.github.com>
2023-11-07 16:11:32 +0300

    odev1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src="https://cdn.sanity.io/images/kts928pd/production/b46a6970cc0f064f5ba5d6370077c7f2e18dbb0f-1200x630.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top" />
+            Bootstrap
+          </a>
+          <a className='link'>ANASAYFA</a>
+          <a className='link'>HAKKIMIZDA</a>
+          <a className='link'>GALERİ</a>
+          <a className='link'>İLETİŞİM</a>
+
+        </div>
+      </nav>
+      <div class="container">
+        <div className='col-5'>
+          <h1>Kategoriler</h1>
+          <li>
+            <ul>asdasf</ul>
+          </li>
+
+        </div>
+
+      </div>
     </div>
   );
 }
